5559c8240e1ad438d796e2aec2aa8db27e9abf7c
Yüce Sungur <yuce@Yuces-MacBook-Air.local>
2020-04-09 00:45:09 +0100

    T

--- app.py
+++ app.py
@@ -16,11 +16,13 @@ def log_request():
 
 @app.route("/")
 def hello():
-    #return "Hello BARIS!2222 "
-    #return "Hello BARIS! " + ", ".join(request.access_route)
-    return "Hello BARIS! " + "https://circleci.com/gh/YuceS/demo1.svg?style=shield"
+    return "Hello BARIS! " + ", ".join(request.access_route)
+    #return "Hello BARIS! " + "https://circleci.com/gh/YuceS/demo1.svg?style=shield"
 
 
+@app.route("/status")
+def badge():
+    return "<HTML><img href=\"https://circleci.com/gh/YuceS/demo1.svg?style=shield\" > </img> </HTML>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug='true',port='9999')
